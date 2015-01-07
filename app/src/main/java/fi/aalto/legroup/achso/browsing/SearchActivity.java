ddb814d83f4d506fd6a9c14e806860ead46be915
Leo Nikkilä <hello@lnikki.la>
2015-01-07 07:49:52 +0200

    Use a switch statement to check the item ID.

--- app/src/main/java/fi/aalto/legroup/achso/browsing/SearchActivity.java
+++ app/src/main/java/fi/aalto/legroup/achso/browsing/SearchActivity.java
@@ -129,14 +129,11 @@ public class SearchActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_about) {
-            return true;
+        switch (id) {
+            case R.id.action_about:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
