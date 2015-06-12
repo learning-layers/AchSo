d16c97ba407635a7d1670a509934c0a84b863be8
bqqbarbhg <samuli.1995@hotmail.com>
2015-06-12 14:34:45 +0300

    Function for merging videos
    Supports two-way and three-way merging

--- app/src/main/java/fi/aalto/legroup/achso/entities/Video.java
+++ app/src/main/java/fi/aalto/legroup/achso/entities/Video.java
@@ -3,9 +3,14 @@ package fi.aalto.legroup.achso.entities;
 import android.location.Location;
 import android.net.Uri;
 
+import com.google.common.base.Objects;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import fi.aalto.legroup.achso.entities.serialization.json.JsonSerializable;
@@ -55,6 +60,76 @@ public class Video implements JsonSerializable {
         this.author = author;
         this.location = location;
         this.annotations = annotations;
+    }
+
+    protected static <T> T mergeValue(T a, T b, T old, boolean preferA) {
+        if (com.google.common.base.Objects.equal(a, b)) {
+            return a;
+        }
+
+        if (old != null) {
+            if (Objects.equal(a, old))
+                return b;
+            if (Objects.equal(b, old))
+                return a;
+        }
+
+        return preferA ? a : b;
+    }
+
+    /**
+     * Merge two video manifests.
+     * @param a The first video to merge.
+     * @param b The second video to merge.
+     * @param old An shared ancestor for the two videos to merge. May be null but results in a worse merge result.
+     */
+    public static Video merge(Video a, Video b, Video old) throws MergeException {
+        boolean preferA = a.getLastModified().getTime() > b.getLastModified().getTime();
+
+        if (!Objects.equal(a.getId(), b.getId())) {
+            throw new MergeException("Trying to merge two unrelated videos");
+        }
+
+        Video video = new Video();
+        video.setId(a.getId());
+        video.setVideoUri(mergeValue(a.getVideoUri(), b.getVideoUri(), old != null? old.getVideoUri() : null, preferA));
+        video.setThumbUri(mergeValue(a.getThumbUri(), b.getThumbUri(), old != null ? old.getThumbUri() : null, preferA));
+        video.setTitle(mergeValue(a.getTitle(), b.getTitle(), old != null ? old.getTitle() : null, preferA));
+        video.setGenre(mergeValue(a.getGenre(), b.getGenre(), old != null ? old.getGenre() : null, preferA));
+        video.setTag(mergeValue(a.getTag(), b.getTag(), old != null ? old.getTag() : null, preferA));
+        video.setDate(mergeValue(a.getDate(), b.getDate(), old != null ? old.getDate() : null, preferA));
+        video.setAuthor(mergeValue(a.getAuthor(), b.getAuthor(), old != null ? old.getAuthor() : null, preferA));
+        video.setLocation(mergeValue(a.getLocation(), b.getLocation(), old != null ? old.getLocation() : null, preferA));
+
+        Set<Annotation> aa = new HashSet<>(a.getAnnotations());
+        Set<Annotation> ab = new HashSet<>(b.getAnnotations());
+        Set<Annotation> ao = old != null ? new HashSet<>(old.getAnnotations()) : Collections.<Annotation>emptySet();
+        Set<Annotation> all = new HashSet<>(aa.size() + ab.size());
+        all.addAll(aa);
+        all.addAll(ab);
+        
+        List<Annotation> merged = new ArrayList<>(aa.size() + ab.size());
+
+        for (Annotation an : all) {
+
+            if (aa.contains(an) && ab.contains(an)) {
+                // Both contain the annotation, so keep it.
+                merged.add(an);
+            }
+
+            // Here the annotation is missing from one of the videos to merge.
+
+            if (ao.contains(an)) {
+                // If the common ancestor contains the annotation it means it wasn't touched in the other one and deleted in the other one, so we can delete it since it wasn't touched in the other video.
+                continue;
+            }
+
+            // The old one doesn't contain it so it is new in one of the videos, keep it.
+            merged.add(an);
+        }
+        video.setAnnotations(merged);
+
+        return video;
     }
 
     /**
