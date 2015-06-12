9fd51bcefdd540747f4c0bf4541edd4a559a9de7
bqqbarbhg <samuli.1995@hotmail.com>
2015-06-12 15:35:28 +0300

    Cleaned up the merging code

--- app/src/main/java/fi/aalto/legroup/achso/entities/Video.java
+++ app/src/main/java/fi/aalto/legroup/achso/entities/Video.java
@@ -62,18 +62,33 @@ public class Video implements JsonSerializable {
         this.annotations = annotations;
     }
 
-    protected static <T> T mergeValue(T a, T b, T old, boolean preferA) {
+    /**
+     * Chooses a value to use in a merged video result.
+     * @param a First option for the value.
+     * @param b Second option for the value.
+     * @param old The common ancestor value of `a` and `b`. Is null in two-way merges.
+     * @param preferA Used for tie-breaking if there isn't enough info to decide will use this.
+     * @return Either the value of `a` or `b` depending on which is the better choice for merging.
+     */
+    protected static <T> T chooseMergeValue(T a, T b, T old, boolean preferA) {
         if (Objects.equal(a, b)) {
+            // Simple case: The values are the same, no need to merge.
             return a;
         }
 
         if (old != null) {
+            // If this is a three-way merge and one of the values is the same as the common
+            // ancestor then we assume that it's unchanged and return the other one.
             if (Objects.equal(a, old))
                 return b;
             if (Objects.equal(b, old))
                 return a;
         }
 
+        // TODO: The tiebreaker could use some kind of a dialog if we want to be super explicit
+        // about losing work
+        // In this case either both have changed his is a two-way merge, the only option is to
+        // use some tiebreaker, for example which file was more recently modified.
         return preferA ? a : b;
     }
 
@@ -82,55 +97,82 @@ public class Video implements JsonSerializable {
      * @param a The first video to merge.
      * @param b The second video to merge.
      * @param old An shared ancestor for the two videos to merge. May be null but results in a
-     *            worse merge result.
+     *            worse merge result. For example annotations can't be deleted in a two-way merge.
      */
     public static Video merge(Video a, Video b, Video old) throws MergeException {
-        boolean preferA = a.getLastModified().getTime() > b.getLastModified().getTime();
 
+        // The UUID:s _must_ match, otherwise the merge doesn't make any sense.
         if (!Objects.equal(a.getId(), b.getId())) {
             throw new MergeException("Trying to merge two unrelated videos");
         }
 
         Video video = new Video();
         video.setId(a.getId());
-        video.setVideoUri(mergeValue(a.getVideoUri(), b.getVideoUri(), old != null? old.getVideoUri() : null, preferA));
-        video.setThumbUri(mergeValue(a.getThumbUri(), b.getThumbUri(), old != null ? old.getThumbUri() : null, preferA));
-        video.setTitle(mergeValue(a.getTitle(), b.getTitle(), old != null ? old.getTitle() : null, preferA));
-        video.setGenre(mergeValue(a.getGenre(), b.getGenre(), old != null ? old.getGenre() : null, preferA));
-        video.setTag(mergeValue(a.getTag(), b.getTag(), old != null ? old.getTag() : null, preferA));
-        video.setDate(mergeValue(a.getDate(), b.getDate(), old != null ? old.getDate() : null, preferA));
-        video.setAuthor(mergeValue(a.getAuthor(), b.getAuthor(), old != null ? old.getAuthor() : null, preferA));
-        video.setLocation(mergeValue(a.getLocation(), b.getLocation(), old != null ? old.getLocation() : null, preferA));
 
-        Set<Annotation> aa = new HashSet<>(a.getAnnotations());
-        Set<Annotation> ab = new HashSet<>(b.getAnnotations());
-        Set<Annotation> ao = old != null ? new HashSet<>(old.getAnnotations()) : Collections.<Annotation>emptySet();
-        Set<Annotation> all = new HashSet<>(aa.size() + ab.size());
-        all.addAll(aa);
-        all.addAll(ab);
-        
-        List<Annotation> merged = new ArrayList<>(aa.size() + ab.size());
+        // Prefer newer values if no other option
+        boolean preferA = a.getLastModified().getTime() > b.getLastModified().getTime();
 
-        for (Annotation an : all) {
+        // Simply choose one of the values for the simple fields (merging title additions changes
+        // be too complicated in scope for this for example)
+        video.setVideoUri(chooseMergeValue(a.getVideoUri(), b.getVideoUri(),
+                old != null ? old.getVideoUri() : null, preferA));
+        video.setThumbUri(chooseMergeValue(a.getThumbUri(), b.getThumbUri(),
+                old != null ? old.getThumbUri() : null, preferA));
+        video.setTitle(chooseMergeValue(a.getTitle(), b.getTitle(),
+                old != null ? old.getTitle() : null, preferA));
+        video.setGenre(chooseMergeValue(a.getGenre(), b.getGenre(),
+                old != null ? old.getGenre() : null, preferA));
+        video.setTag(chooseMergeValue(a.getTag(), b.getTag(),
+                old != null ? old.getTag() : null, preferA));
+        video.setDate(chooseMergeValue(a.getDate(), b.getDate(),
+                old != null ? old.getDate() : null, preferA));
+        video.setAuthor(chooseMergeValue(a.getAuthor(), b.getAuthor(),
+                old != null ? old.getAuthor() : null, preferA));
+        video.setLocation(chooseMergeValue(a.getLocation(), b.getLocation(),
+                old != null ? old.getLocation() : null, preferA));
 
-            if (aa.contains(an) && ab.contains(an)) {
-                // Both contain the annotation, so keep it.
-                merged.add(an);
+        // Put all the annotations in Sets so we can quickly check for identical ones.
+        Set<Annotation> annotationsA = new HashSet<>(a.getAnnotations());
+        Set<Annotation> annotationsB = new HashSet<>(b.getAnnotations());
+        Set<Annotation> annotationsOld = old != null ? new HashSet<>(old.getAnnotations())
+                : Collections.<Annotation>emptySet();
+
+        int reservedAnnotationCount = annotationsA.size() + annotationsB.size();
+
+        List<Annotation> mergedAnnotations = new ArrayList<>(reservedAnnotationCount);
+        Set<Annotation> annotationsAll = new HashSet<>(reservedAnnotationCount);
+
+        // Because `annotationsAll` is a Set we don't have to worry about duplicate Annotations.
+        annotationsAll.addAll(annotationsA);
+        annotationsAll.addAll(annotationsB);
+
+        // Iterate through every annotation in `a` and `b`
+        for (Annotation annotation : annotationsAll) {
+
+            // annotation: An annotation which is in at least one of the videos to merge
+
+            if (annotationsA.contains(annotation) && annotationsB.contains(annotation)) {
+                // Both videos contain the annotation, so keep it for sure.
+                mergedAnnotations.add(annotation);
             }
 
             // Here the annotation is missing from one of the videos to merge.
 
-            if (ao.contains(an)) {
+            if (annotationsOld.contains(annotation)) {
                 // If the common ancestor contains the annotation it means it wasn't touched in
                 // the other one and deleted in the other one, so we can delete it since it
                 // wasn't touched in the other video.
                 continue;
             }
 
-            // The old one doesn't contain it so it is new in one of the videos, keep it.
-            merged.add(an);
+            // The old one doesn't contain it so it is new in one of the videos, keep it. This
+            // can result in double annotations in the case that two people for example move a
+            // single annotation at the same time, but this should be rare and this way it's
+            // impossible to lose work due to merging.
+            mergedAnnotations.add(annotation);
         }
-        video.setAnnotations(merged);
+
+        video.setAnnotations(mergedAnnotations);
 
         return video;
     }
