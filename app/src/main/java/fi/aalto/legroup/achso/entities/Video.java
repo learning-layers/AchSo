8c7a35a85a3bc9a0e49443a553b89872b31667e0
bqqbarbhg <samuli.1995@hotmail.com>
2015-10-28 17:36:05 +0200

    Remove utility function used by removed merge

--- app/src/main/java/fi/aalto/legroup/achso/entities/Video.java
+++ app/src/main/java/fi/aalto/legroup/achso/entities/Video.java
@@ -60,37 +60,7 @@ public class Video implements JsonSerializable {
         this.location = location;
         this.annotations = annotations;
     }
-
-    /**
-     * Chooses a value to use in a merged video result.
-     * @param a First option for the value.
-     * @param b Second option for the value.
-     * @param old The common ancestor value of `a` and `b`. Is null in two-way merges.
-     * @param preferA Used for tie-breaking if there isn't enough info to decide will use this.
-     * @return Either the value of `a` or `b` depending on which is the better choice for merging.
-     */
-    protected static <T> T chooseMergeValue(T a, T b, T old, boolean preferA) {
-        if (Objects.equal(a, b)) {
-            // Simple case: The values are the same, no need to merge.
-            return a;
-        }
-
-        if (old != null) {
-            // If this is a three-way merge and one of the values is the same as the common
-            // ancestor then we assume that it's unchanged and return the other one.
-            if (Objects.equal(a, old))
-                return b;
-            if (Objects.equal(b, old))
-                return a;
-        }
-
-        // TODO: The tiebreaker could use some kind of a dialog if we want to be super explicit
-        // about losing work
-        // In this case either both have changed his is a two-way merge, the only option is to
-        // use some tiebreaker, for example which file was more recently modified.
-        return preferA ? a : b;
-    }
-
+    
     /**
      * Convenience method for saving a video.
      * @return True if succeeded, false otherwise.
