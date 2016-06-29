b4ba834d0e27a0c01b8fb982f52ec1654b5930a6
Matti Jokitulppo <melonmanchan@gmail.com>
2016-06-29 17:07:32 +0300

    More work on video exporting helper class

--- app/src/main/java/fi/aalto/legroup/achso/authoring/ExportHelper.java
+++ app/src/main/java/fi/aalto/legroup/achso/authoring/ExportHelper.java
@@ -1,16 +1,21 @@
 package fi.aalto.legroup.achso.authoring;
 
+import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
+import fi.aalto.legroup.achso.app.App;
 import fi.aalto.legroup.achso.entities.serialization.json.JsonSerializable;
 import fi.aalto.legroup.achso.entities.Video;
 import fi.aalto.legroup.achso.entities.serialization.json.JsonSerializer;
 
+import android.accounts.Account;
 import android.net.Uri;
 import android.text.TextUtils;
 import android.util.Patterns;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ExportHelper {
@@ -20,10 +25,19 @@ public class ExportHelper {
     public static class ExportPayload implements JsonSerializable {
         public String email;
         public ArrayList<Video> videos;
+
+        public ExportPayload(String email, ArrayList<Video> videos) {
+            this.email  = email;
+            this.videos = videos;
+        }
     }
 
     public static class ExportResponse implements JsonSerializable {
         public String message;
+
+        public ExportResponse(String message) {
+            this.message = message;
+        }
     }
 
     public ExportHelper(JsonSerializer serializer, Uri endpointUri) {
@@ -31,10 +45,26 @@ public class ExportHelper {
         this.endpointUri = endpointUri;
     }
 
-    public void exportVideos(ArrayList<Video> videos, String email) {
+    public ExportResponse exportVideos(ArrayList<Video> videos, String email) throws IOException {
         if (!isValidEmail(email)) {
-            return;
+            throw new IOException("Invalid email address: " + email);
         }
+
+        ExportPayload exportPayload = new ExportPayload(email, videos);
+
+        String serializedPayload = serializer.write(exportPayload);
+        Request request = new Request.Builder()
+                .url(endpointUri.toString())
+                .post(RequestBody.create(MediaType.parse("application/json"), serializedPayload))
+                .build();
+
+        Account account = App.loginManager.getAccount();
+        Response response = App.authenticatedHttpClient.execute(request, account);
+        if (!response.isSuccessful())
+            throw new IOException(response.body().string());
+        ExportResponse result = serializer.read(ExportResponse.class, response.body().byteStream());
+
+        return new ExportResponse(result.message);
 
     }
 
