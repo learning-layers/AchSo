4e594a10fde1b87ba598ec862c546c3c329bec07
Matti Jokitulppo <melonmanchan@gmail.com>
2016-08-15 17:49:47 +0300

    Add heart icon to tile to indicate being favorited

--- app/src/main/java/fi/aalto/legroup/achso/views/adapters/VideoGridAdapter.java
+++ app/src/main/java/fi/aalto/legroup/achso/views/adapters/VideoGridAdapter.java
@@ -93,6 +93,12 @@ public final class VideoGridAdapter extends RecyclerView.Adapter<VideoGridAdapte
             holder.getUploadIndicator().setImageAlpha(0xFF);
             holder.getUploadIndicator().setImageResource(R.drawable.ic_cloud_done_white_24dp);
         }
+
+        if (video.hasCachedFiles()) {
+            holder.getFavoriteIndicator().setImageAlpha(0xFF);
+        } else {
+            holder.getFavoriteIndicator().setImageAlpha(0x00);
+        }
     }
 
     @Override
@@ -169,6 +175,7 @@ public final class VideoGridAdapter extends RecyclerView.Adapter<VideoGridAdapte
         private TextView titleText;
         private ImageView thumbImage;
         private ImageView uploadIndicator;
+        private ImageView favoriteIndicator;
         private ProgressBar progressBar;
         private View selectionOverlay;
 
@@ -179,6 +186,7 @@ public final class VideoGridAdapter extends RecyclerView.Adapter<VideoGridAdapte
             this.titleText = (TextView) view.findViewById(R.id.titleText);
             this.thumbImage = (ImageView) view.findViewById(R.id.thumbImage);
             this.uploadIndicator = (ImageView) view.findViewById(R.id.uploadButton);
+            this.favoriteIndicator = (ImageView) view.findViewById(R.id.favoriteButton);
             this.progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
             this.selectionOverlay = view.findViewById(R.id.selectionOverlay);
         }
@@ -193,6 +201,10 @@ public final class VideoGridAdapter extends RecyclerView.Adapter<VideoGridAdapte
 
         public ImageView getThumbImage() {
             return this.thumbImage;
+        }
+
+        public ImageView getFavoriteIndicator () {
+            return this.favoriteIndicator;
         }
 
         public ImageView getUploadIndicator() {
